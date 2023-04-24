ab1802afb44b4c8996390b08304334e4a491d3fc
Laplace-bit <dzenglin@gmail.com>
2023-04-24 23:06:47 +0800

    调整模型相机

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,19 +2,24 @@ import React, { useEffect, useRef, useLayoutEffect, useState } from 'react';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
-// import Stats from 'three/examples/jsm/libs/stats.module';
 const Home: React.FC = () => {
 
     const continator = useRef<HTMLDivElement>(null);
+
     let model: any = useState(null);
     const scene = new THREE.Scene();
     const renderer = new THREE.WebGLRenderer({
         // 抗锯齿性
         antialias: true
     });
-    const camera = new THREE.PerspectiveCamera(200, window.innerWidth / window.innerHeight, 0.1, 500);
-
+    renderer.outputEncoding = THREE.sRGBEncoding;
+    const fov = 70 // 视野范围
+    const aspect = 2 // 相机默认值 画布的宽高比
+    const near = 1 // 近平面
+    const far = 2000 // 远平面
+    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera.position.set(0, 0, 0);
+    // camera.setViewOffset(1200, 900, 0, 0, 150, 250);
 
     const loadModel = () => {
         // Load a glTF resource
@@ -25,8 +30,9 @@ const Home: React.FC = () => {
             // called when the resource is loaded
             (gltf) => {
                 model = gltf.scene;
-                model.scale.set(1, 1, 1);
-                model.position.y = 1;
+                model.scale.set(0.6, 0.6, 0.5);
+                model.position.y = -3.5;
+                model.position.x = -1;
                 scene.add(model);
             },
             // called while loading is progressing
@@ -42,13 +48,15 @@ const Home: React.FC = () => {
         // 特别注意，相机的位置要大于几何体的尺寸
         camera.position.z = -10;
         // 设置背景色
-        renderer.setClearColor('#428bca', 2);
-        renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.setClearColor('#cccccc', 2);
+        renderer.setSize(continator.current?.offsetWidth || 500, continator.current?.offsetHeight || 500);
         continator.current?.appendChild(renderer.domElement);
-
         // 环境光不能用来投射阴影，因为它没有方向。
-        var ambienLight = new THREE.AmbientLight(0xcccccc);
-        scene.add(ambienLight);
+        var ambienLight = new THREE.AmbientLight(0xffffff, 0.5);
+        scene.add(ambienLight,);
+        var directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+        scene.add(directionalLight);
+
         // 初始化轨道控制器
         // 还需要配合animate，不断循环渲染，达到用鼠标旋转物体的作用。
         const orbitControls = new OrbitControls(camera, renderer.domElement);
@@ -57,17 +65,18 @@ const Home: React.FC = () => {
     }
 
     function onWindowResize() {
-        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.aspect = 1.5;
+        // camera.aspect = continator.current?.offsetWidth  / continator.current?.offsetHeight ;
         camera.updateProjectionMatrix();
-        renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.setSize(continator.current?.offsetWidth || 500, continator.current?.offsetHeight || 500);
     }
 
     function animate() {
         setTimeout(() => {
-            model.rotation.x += 0.01;
+            // model.rotation.x += 0.01;
             model.rotation.y += 0.01;
-            model.rotation.z += 0.01;
-        }, 500)
+            // model.rotation.z += 0.01;
+        }, 1000)
         requestAnimationFrame(animate);
         renderer.render(scene, camera);
     }
